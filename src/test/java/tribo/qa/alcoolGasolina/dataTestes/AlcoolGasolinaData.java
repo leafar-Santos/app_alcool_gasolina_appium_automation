3c25c6df02c3cda6df598a75a715aed0c5cf7b9d
leafar-Santos <leafarccomputacao@gmail.com>
2025-02-23 23:44:14 -0300

    Ajuste valores para variáveis

---
+++ src/test/java/tribo/qa/alcoolGasolina/dataTestes/AlcoolGasolinaData.java
@@ -0,0 +1,9 @@
+package tribo.qa.alcoolGasolina.dataTestes;
+
+public class AlcoolGasolinaData {
+
+   public static String precoAlto = "10.0";
+   public static String precoBaixo = "5.0";
+   public static String nonValue = "";
+
+}
--- src/test/java/tribo/qa/alcoolGasolina/test/AlcoolGasolinaTest.java
+++ src/test/java/tribo/qa/alcoolGasolina/test/AlcoolGasolinaTest.java
@@ -8,6 +8,7 @@ import tribo.qa.alcoolGasolina.core.DriverFactory;
 import tribo.qa.alcoolGasolina.page.AlcoolGasolinaPage;
 
 
+import static tribo.qa.alcoolGasolina.dataTestes.AlcoolGasolinaData.*;
 import static tribo.qa.alcoolGasolina.dataTests.epics.AlcoolGasolinaEpic.createAddressPage;
 import static tribo.qa.alcoolGasolina.dataTests.histories.AlcoolGasolinaStory.alcoolGasolinaStory;
 
@@ -26,11 +27,11 @@ public class AlcoolGasolinaTest extends BaseTest {
     @Epic(createAddressPage)
     @Story(alcoolGasolinaStory)
     @Severity(SeverityLevel.CRITICAL)
-    @DisplayName("Deve valida que que a melhor para uso é o àlcool")
+    @DisplayName("Deve validar que a melhor para uso é o àlcool")
     @Description("Inserir preço do alcool menor do que o preço da gasolina")
     public void deveCalcularMelhorOpcaoAlcool()  {
-        page.informarAlcool("10.4");
-        page.informarGasolina("16.7");
+        page.informarAlcool(precoBaixo);
+        page.informarGasolina(precoAlto);
         page.clicarCalcular();
         page.verificarResultado("Melhor utilizar Álcool!");
     }
@@ -42,8 +43,8 @@ public class AlcoolGasolinaTest extends BaseTest {
     @Description("Inserir preço do alcool maior do que o preço da gasolina")
     @DisplayName("Deve validaque que a melhor para uso é a gasolina")
     public void deveCalcularMelhorOpcaoGasolina() {
-        page.informarAlcool("20.4");
-        page.informarGasolina("10.7");
+        page.informarAlcool(precoAlto);
+        page.informarGasolina(precoBaixo);
         page.clicarCalcular();
         page.verificarResultado("Melhor utilizar Gasolina!");
     }
@@ -55,8 +56,8 @@ public class AlcoolGasolinaTest extends BaseTest {
     @Description("Deixar os campos em branco e tentar calcular.")
     @DisplayName("Deve retornar mensagem de falha ao deixar campos vazios e clicar em Calcular")
     public void deveRetornarMensagemCamposVazios() {
-        page.informarAlcool("");
-        page.informarGasolina("");
+        page.informarAlcool(nonValue);
+        page.informarGasolina(nonValue);
         page.clicarCalcular();
         page.verificarResultado("Preencha os preços primeiro!");
     }
@@ -68,16 +69,21 @@ public class AlcoolGasolinaTest extends BaseTest {
     @Description("Deixar o campo alcool vazio e preencher o campo gasolina.")
     @DisplayName("Deve retornar mensagem de falha ao deixar campo álcool vazio clicar em Calcular")
     public void deveRetornarMensagemCampoAlcoolVazio() {
-        page.informarAlcool("");
-        page.informarGasolina("10.2");
+        page.informarAlcool(nonValue);
+        page.informarGasolina(precoAlto);
         page.clicarCalcular();
         page.verificarResultado("Preencha os preços primeiro!");
     }
 
     @Test
+    @Epic(createAddressPage)
+    @Story(alcoolGasolinaStory)
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Deixar o campo gasolina vazio e preencher o campo alcool.")
+    @DisplayName("Deve retornar mensagem de falha ao deixar campo álcool vazio clicar em Calcular")
     public void deveRetornarMensagemCampoGasolinalVazio(){
-        page.informarAlcool("10.2");
-        page.informarGasolina("");
+        page.informarAlcool(precoAlto);
+        page.informarGasolina(nonValue);
         page.clicarCalcular();
         page.verificarResultado("Preencha os preços primeiro!");
     }
